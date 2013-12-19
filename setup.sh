c66f16bb59463b9de237a6337559e9f0026992cb
carlineng <carlineng@gmail.com>
2013-12-19 14:32:19 -0800

    Change to use my own dotfile repo

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/carlineng/dotfiles.git
 ln -sb dotfiles/.vimrc .
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
