9c962d96e0fd01b753c203db6e304d7f1716a62d
carlineng <carlineng@gmail.com>
2014-01-28 18:32:36 -0800

    Create setup-analytics.sh

---
+++ setup-analytics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+sudo apt-get install -y python-pip
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/carlineng/dotfiles.git
+ln -sb dotfiles/.vimrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
